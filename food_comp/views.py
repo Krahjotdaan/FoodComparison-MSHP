47bafaa34358a823e8c9205f0e84a98b6e66b160
Alex Vititnev <al@al.al>
2023-03-05 19:57:38 +0300

    Added index page, Type: Build

--- food_comp/views.py
+++ food_comp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from food_comp import models
+
+def index(request):
+    context = dict()
+    context["text"] = "фруктовый ввод..."
+    return render(request, "index.html", context)
+
 
 # Create your views here.
