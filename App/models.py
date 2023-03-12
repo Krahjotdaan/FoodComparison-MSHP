0c46e6c7d3b2b57627ddaf932f35b3edca622b26
Ostapenko <Student@POKR-R601-STD11.staff.corp.local>
2023-03-12 17:56:57 +0300

    Feat: added class Like

--- App/models.py
+++ App/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Fruit:
+    pass
+
+
+class Like(models.Model):
+    fruit = models.ForeignKey(to=Fruit, default=1, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
+
+    @staticmethod
+    def add(fruit, author):
+        Like.objects.create(
+            fruit=fruit,
+            author=author
+        )
+
+    @staticmethod
+    def get():
+        return Like.objects.all()
