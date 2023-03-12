4c51a7bf162e067c5ce9469a97f7a04bbefbd47f
Ivan_ <razvodovivanok@outlook.com>
2023-03-12 18:14:20 +0300

    feat: Added class Vitamin in models.py

--- App/models.py
+++ App/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 # Create your models here.
+
+class Vitamin(models.Model):
+    vitamin_id = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    @staticmethod
+    def get():
+        return Vitamin.objects.all()
+
+    @staticmethod
+    def add(self, vitamin_id, name):
+        Vitamin.objects.create(
+            vitamin_id=vitamin_id,
+            name=name
+        )
+        messages.success(self.request, 'Name added')
