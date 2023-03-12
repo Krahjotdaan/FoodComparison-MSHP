cd6dc026fde2fdf8daa5c0d757fa076aff6102db
Студент НИУ ВШЭ <Student@POKR-R601-STD09.staff.corp.local>
2023-03-12 18:09:33 +0300

    Type: feat; Facts
    БД интересные факты добавлена

--- App/models.py
+++ App/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Fact(models.Model):
+    # Таблица интересных фактов
+    id = models.IntegerField()
+    food = models.OneToOneField(to=Food, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1000)
+
