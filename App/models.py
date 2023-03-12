adde2b4233c9f62d303642904429501b8aedf1f1
morevq <zhenulik5@gmail.com>
2023-03-12 19:14:33 +0300

    style: delete id and table food to top

--- App/models.py
+++ App/models.py
@@ -2,23 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
+    searched = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    deathdoze = models.IntegerField()
 
 class VitaminFood(models.Model):
     #Таблица витамина в еде
-    id = models.IntegerField()
     vitamin = models.OneToOneField(to=Vitamin, on_delete=models.CASCADE)
     food = models.OneToOneField(to=Food, on_delete=models.CASCADE)
 
 
 class Fact(models.Model):
     # Таблица интересных фактов
-    id = models.IntegerField()
     food = models.OneToOneField(to=Food, on_delete=models.CASCADE)
     description = models.CharField(max_length=1000)
 
 
 class Vitamin(models.Model):
-    vitamin_id = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
     @staticmethod
@@ -32,13 +35,5 @@ class Vitamin(models.Model):
             name=name
         )
         messages.success(self.request, 'Name added')
-
-class Food(models.Model):
-    food_id = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    author = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
-    searched = models.IntegerField()
-    description = models.CharField(max_length=1000)
-    deathdoze = models.IntegerField()
 
 
