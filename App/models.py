3c5858ddec89c8c66016057b63ed56d947aa0def
Nikita Naurskii <wduck223@yandex.ru>
2023-03-12 19:03:21 +0300

    feat: db fruit

--- App/models.py
+++ App/models.py
@@ -5,3 +5,13 @@ class VitaminFood(models.Model):
     id = models.IntegerField()
     vitamin = models.OneToOneField(to=Vitamin, on_delete=models.CASCADE)
     food = models.OneToOneField(to=Food, on_delete=models.CASCADE)
+
+class Food(models.Model):
+    food_id = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(to=User, default=1, on_delete=models.CASCADE)
+    searched = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    deathdoze = models.IntegerField()
+
+
