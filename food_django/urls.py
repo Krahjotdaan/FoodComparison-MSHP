aa19afae1d8aa8d691aa0b5ed7673c45450b9c36
Миркин Алексей <managestudylexa@gmail.com>
2023-04-02 08:56:29 +0300

    refactor: renamed page function to profile_page

--- food_django/urls.py
+++ food_django/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('food/', views.food_list_page, name='food_list'),
     path('food_item/', views.food_item_page, name='food_item'),
-    path('profile/page.html', views.page, name='profile_page'),
+    path('profile/page.html', views.profile_page, name='profile_page'),
 ]
